3780e9b50bf7d1d5a66b6f52eb0fae7f19992119
otiai10 <otiai10@gmail.com>
2015-08-15 00:27:59 +0900

    Fix page

--- pages/js/release-info.js
+++ pages/js/release-info.js
@@ -1,6 +1,5 @@
 $(function(){
-    $.get("https://cdn.rawgit.com/otiai10/kanColleWidget/develop/README.md", function(res) {
-        var info = res.split("- RELEASEINFO\n")[1];
-        $("#release-note").text(info);
+    $.get("https://cdn.rawgit.com/otiai10/kanColleWidget/develop/RELEASENOTES.md", function(res) {
+        $("#release-note").text(res);
     });
 });
